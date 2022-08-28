c85449257454f7131927904eb5e6f03ec30ade24
Matt Krueger <mkrueger@rstms.net>
2022-08-28 13:17:31 -0500

    Bump version: 0.0.1 → 0.0.2

--- python_boilerplate/version.py
+++ python_boilerplate/version.py
@@ -1,4 +1,4 @@
-__version__ = "0.0.1"
+__version__ = "0.0.2"
 __author__ = "Matt Krueger"
 __email__ = "mkrueger@rstms.net"
-__timestamp__ = "2022-08-28 13:12:42-05:00"
+__timestamp__ = "2022-08-28 13:17:31-05:00"
